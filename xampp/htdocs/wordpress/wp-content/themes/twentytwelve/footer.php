6fa6020951da190c1290bdac86a43b7a1d230ecb
Augusto Serrao <augusto_moya_serrao@hotmail.com>
2018-03-02 15:29:10 -0500

    [Soccer World Cup website] Added pages and posts using downloaded pictures and plugins.

---
+++ xampp/htdocs/wordpress/wp-content/themes/twentytwelve/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<h3>Send us an email:</h3>
+		<a href="mailto:augustoserrao@soccerworldcup.com">augustoserrao@soccerworldcup.com</a>
+		<br>
+		<br>
+		<h1>Copyright &copy; 2018 - All Rights Reserved - www.soccerworldcup.com - Soccer World Cup Association</h1>
+		<br>
+		<div class="site-info">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
