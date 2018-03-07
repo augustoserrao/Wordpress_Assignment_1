50cccd2069da2b0a49e20726982501901509f601
Augusto Serrao <augusto_moya_serrao@hotmail.com>
2018-03-07 10:08:43 -0500

    [Wordpress Assignment 1] Use of new plugins

---
+++ xampp/htdocs/wordpress/wp-content/plugins/simple-links/admin-views/pro-notice.php
@@ -0,0 +1,8 @@
+<fieldset style="border: 1px solid black; border-radius: 10px; padding: 0 10px; margin: 10px 0 0 0;">
+	<legend style="font-size: 14px; font-weight: bold;">
+		<?php _e( 'Checkout Simple Links Pro!', 'simple-links' ); ?>
+	</legend>
+    <p style="font-size: 14px; margin: 0; padding: 5px">
+		<?php printf( __( 'Upgrade to %sSimple Links Pro%s for Priority Support, Display By Category, Import/Export Ability, Links In Search Results, and so much more!', 'simple-links' ), '<a target="blank" href="https://matlipe.com/product/simple-links-pro/">', '</a>' ); ?>
+    <p>
+</fieldset>
